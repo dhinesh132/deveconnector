c851d8c2f08768ab89e063965d2e974c7cc28364
Dhinesh <dhinesh@etechnologycentre.com>
2019-01-23 23:13:02 +0800

    Profile Add/Edit and Validation

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,6 +1,95 @@
 const express = require("express");
 const route = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const validateProfileInput = require("../../validator/profile");
+
+//load Profile Model
+const Profile = require("../../models/Profile");
+
+const User = require("../../models/User");
 
 route.get("/test", (req, res) => res.json({ msg: "Profiles API Works" }));
+
+//@route get api/profile
+//@desc current user profile
+//@access Private
+
+route.get("/", passport.authenticate("jwt", { session: false }), (req, res) => {
+  const errors = {};
+  Profile.findOne({ user: req.user.id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "Profile not exist";
+        return res.status(404).json(errors);
+      } else {
+        res.json(profile);
+      }
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+//@route Post api/profile
+//@desc current user profile
+//@access Private
+
+route.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.status) profileFields.status = req.body.status;
+    //Skill array field
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(",");
+    }
+
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+
+    profileFields.social = {};
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        //update
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        //create
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = "Handle already exist";
+            res.status(400).json(errors);
+          }
+          new Profile(profileFields).save().then(profile => {
+            res.json(profile);
+          });
+        });
+      }
+    });
+  }
+);
 
 module.exports = route;
